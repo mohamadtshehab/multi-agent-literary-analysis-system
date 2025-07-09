4adca250439ac99017f4b4ab197c597020736072
Mohamad Shehab <iammohamadshehab@gmail.com>
2025-07-09 16:55:40 +0300

    added graph visualization

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ from dotenv import load_dotenv
 from src.graphs import compiled_graph
 from src.states import initial_state
 from src.configs import config
+from src.visualize_graph import visualize_graph
 
 load_dotenv()
 
-response = compiled_graph.invoke(initial_state, config=config)
+
+visualize_graph(compiled_graph)
+# response = compiled_graph.invoke(initial_state, config=config)
+
+
 
 if __name__ == "__main__":
-    print(response)
+    # print(response)
+    pass
